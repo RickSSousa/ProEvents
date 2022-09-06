48ef8a51e7097c8335f43305d070d10bfc9372a4
Ricardo Santos de Sousa <ricardo.sousa@legalcontrol.com.br>
2022-09-06 00:33:55 -0300

    First Migrations and database

--- Back/src/ProEvents.API/Controllers/EventController.cs
+++ Back/src/ProEvents.API/Controllers/EventController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using ProEvents.API.Data;
 using ProEvents.API.Models;
 
 namespace ProEvents.API.Controllers
@@ -11,41 +12,24 @@ namespace ProEvents.API.Controllers
     public class EventController : ControllerBase
     {
 
-        public IEnumerable<Event> _event = new Event[]{
-            new Event(){
-                EventId = 1,
-                Theme = "Angular 11 e .NET 5",
-                Local = "Alfenas",
-                AmountPeople = 250,
-                EventDate = DateTime.Now.AddDays(2).ToString(),
-                Lot = "1º Lote",
-                ImageURL = "foto.png"
-            },
-            new Event(){
-                EventId = 2,
-                Theme = "Angular e suas novidades",
-                Local = "Belo Horizonte",
-                AmountPeople = 350,
-                EventDate = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-                Lot = "2º Lote",
-                ImageURL = "foto1.png"
-            }
-        };
-        public EventController()
+        private readonly DataContext _context;
+        
+        public EventController(DataContext context)
         {
+            _context = context;
         }
 
         [HttpGet]
         public IEnumerable<Event> Get()
         {
-            return _event;
+            return _context.Events;
         }
         
         [HttpGet("{id}")]
-        public IEnumerable<Event> GetById(int id)
+        public Event GetById(int id)
         {
             //retorna o event onde o id desse evente é igual ao do parâmetro
-            return _event.Where(e => e.EventId == id);
+            return _context.Events.FirstOrDefault(e => e.EventId == id);
         }
     }
 }
