0d18141e29efec9b453e6590496a75b00b976d91
Ricardo Santos de Sousa <ricardo.sousa@legalcontrol.com.br>
2022-09-08 23:12:53 -0300

    implemented getEvents method with static data

--- Front/ProEvents-App/src/app/eventos/eventos.component.ts
+++ Front/ProEvents-App/src/app/eventos/eventos.component.ts
@@ -3,13 +3,32 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-eventos',
   templateUrl: './eventos.component.html',
-  styleUrls: ['./eventos.component.scss']
+  styleUrls: ['./eventos.component.scss'],
 })
 export class EventosComponent implements OnInit {
+  public eventos: any;
 
-  constructor() { }
+  constructor() {}
 
+  //esse método chama nosso getEventos antes da aplicação interpretar o html, é tipo o useEffect
   ngOnInit(): void {
+    this.getEventos;
   }
 
+  public getEventos(): void {
+    this.eventos = [
+      {
+        Tema: 'Angular 11',
+        Local: 'Seattle',
+      },
+      {
+        Tema: '.NET 5',
+        Local: 'New York',
+      },
+      {
+        Tema: 'Angular e suas novidades',
+        Local: 'Boston',
+      },
+    ];
+  }
 }
