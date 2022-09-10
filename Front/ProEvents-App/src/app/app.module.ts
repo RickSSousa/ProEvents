18292e1a0e602b2cabec8f948ec1b3481a6d6d90
Ricardo Santos de Sousa <ricardo.sousa@legalcontrol.com.br>
2022-09-10 12:31:25 -0300

    Addition of bootstrap and a navbar

--- Front/ProEvents-App/src/app/app.module.ts
+++ Front/ProEvents-App/src/app/app.module.ts
@@ -6,13 +6,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { SpeakersComponent } from './speakers/speakers.component';
+import { NavComponent } from './nav/nav.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 @NgModule({
-  declarations: [AppComponent, EventosComponent, SpeakersComponent],
+  declarations: [
+    AppComponent,
+    EventosComponent,
+    SpeakersComponent,
+    NavComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule, //respondável por importar o tipo necessário pra chamada da api no ts dos componentes
+    BrowserAnimationsModule,
+    CollapseModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent],
