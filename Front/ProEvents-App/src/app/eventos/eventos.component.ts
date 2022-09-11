5cd56fb358d3e47555cba4eb9548b876c51dce3a
Ricardo Santos de Sousa <ricardo.sousa@legalcontrol.com.br>
2022-09-11 12:39:59 -0300

    added filter by theme and location and removed old comments

--- Front/ProEvents-App/src/app/eventos/eventos.component.ts
+++ Front/ProEvents-App/src/app/eventos/eventos.component.ts
@@ -8,26 +8,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
   public events: any = [];
+  //a variável abaixo é necessária para que, quando os eventos forem filtrados e eu apagar o filtro do input, ele não atribuir ao array events algo vazio, o q n retornaria nada. fazendo um outro array, eu posso atribuir a ele todos os eventos trazidos pelo event
+  public eventsFiltered: any = [];
   marginImg: number = 2;
   viewImg: boolean = true;
-  listFilter: string = '';
+
+  private _listFilter: string = '';
+
+  public get listFilter(): string {
+    return this._listFilter;
+  }
+
+  public set listFilter(value: string) {
+    this._listFilter = value;
+    //para filtrar, se houver algo na variável filtro de eventos, vou chamar a função filtrar eventos passando como parâmetro o evento que quero filtrar, se não retorna tds os eventos
+    this.eventsFiltered = this.listFilter
+      ? this.filterEvents(this.listFilter)
+      : this.events;
+  }
+
+  filterEvents(filterBy: string): any {
+    filterBy = filterBy.toLocaleLowerCase(); //dxando td em letras minúsculas
+    return this.events.filter(
+      //a cada evento, dentro de eventos, to pegando o tema ou local, pondo tudo em letras minúsculas e filtrando pelo meu parâmetro de entrada
+      (event: any) =>
+        event.theme.toLocaleLowerCase().indexOf(filterBy) !== -1 ||
+        event.local.toLocaleLowerCase().indexOf(filterBy) !== -1
+    );
+  }
 
   constructor(private http: HttpClient) {}
 
-  //esse método chama nosso getEventos antes da aplicação interpretar o html, é tipo o useEffect
   ngOnInit(): void {
-    this.getEventos();
+    this.getEvents();
   }
 
   alterImgState() {
     this.viewImg = !this.viewImg;
   }
 
-  public getEventos(): void {
-    //chamando a api para pegar a resposta e atribuir na variavel eventos tds os eventos
+  public getEvents(): void {
     this.http.get('https://localhost:5001/api/event').subscribe(
-      (response) => (this.events = response), //trás os eventos da api
-      (error) => console.log(error) //escreve algum erro q der
+      (response) => {
+        this.events = response;
+        this.eventsFiltered = response; //aqui é só pra eu popular essa variavel msm, pra aparecer tds os eventos quando eu carregar a pag d primeira
+      },
+      (error) => console.log(error)
     );
   }
 }
